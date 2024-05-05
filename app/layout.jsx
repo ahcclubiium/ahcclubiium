8ad68807686bdba8415672207496105b78bc8db2
hakimnazry <hakimnazry@gmail.com>
2024-05-05 22:30:49 +0800

    add metatag for seo

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,21 +7,40 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "Motion-U Club Official Website",
-	description: "This is Motion-U Club Official Website",
+  title: "Motion-U Club Official Website",
+  description: "This is Motion-U Club Official Website",
+  keywords: [
+    "technopreneurship",
+    "technopreneurship club",
+    "entrepreneurship",
+    "entrepreneurship club",
+    "motionu",
+    "motionu club",
+    "sig motionu",
+    "motionu iium",
+    "motionu kict",
+    "motionu kict iium",
+    "motion-u",
+    "motion-u club",
+    "sig motion-u",
+    "motion-u iium",
+    "motion-u kict",
+    "motion-u kict iium",
+  ],
+  metadataBase: new URL("https://www.motionu.club/"),
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body
-				className={`${inter.className} overflow-x-hidden relative max-w-[100vw]`}
-			>
-				<Header />
-				{children}
-				{/* <Footer /> */}
-				<FooterNew />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} overflow-x-hidden relative max-w-[100vw]`}
+      >
+        <Header />
+        {children}
+        {/* <Footer /> */}
+        <FooterNew />
+      </body>
+    </html>
+  );
 }
