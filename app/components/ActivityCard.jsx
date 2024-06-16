c9d5e8a5548e1894fc233b46df40504489a0a29d
friessama <fs561317@gmail.com>
2024-06-16 20:24:47 +0800

    Update

--- app/components/ActivityCard.jsx
+++ app/components/ActivityCard.jsx
@@ -1,6 +1,6 @@
 export default function ActivityCard({ activityImage, activityTitle, activityDesc }) {
   return (
-    <div className="flex flex-col items-center justify-center w-64 space-y-5 p-3 px-6 bg-[#FD775D] rounded-3xl border-2 border-black">
+    <div className="flex flex-col items-center justify-center w-72 space-y-12 p-5 px-8 bg-[#FD775D] rounded-3xl border-2 border-black">
       <div className="text-6xl">
         {activityImage}
       </div>
