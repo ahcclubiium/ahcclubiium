0cc8265f16116ab768b0db1eb37a0ed280b8d5df
PlashSpeed-Aiman <62431177+PlashSpeed-Aiman@users.noreply.github.com>
2024-07-06 00:46:11 +0800

    Update page.jsx

--- app/program_cms/[slug]/page.jsx
+++ app/program_cms/[slug]/page.jsx
@@ -7,6 +7,7 @@ import { createClient } from "contentful";
 export default async function News({ params }) {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
+    environment: "master", // defaults to 'master' if not set
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
   });
 
