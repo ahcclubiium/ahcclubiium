1e96937d06e7fe5333bb89c83c55868fd365aa6a
PlashSpeed-Aiman <62431177+PlashSpeed-Aiman@users.noreply.github.com>
2024-07-06 01:00:08 +0800

    Update page.jsx

--- app/program_cms/[slug]/page.jsx
+++ app/program_cms/[slug]/page.jsx
@@ -6,9 +6,9 @@ import { createClient } from "contentful";
 
 export default async function News({ params }) {
   const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
+    space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
     environment: "master", // defaults to 'master' if not set
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+    accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
   });
 
   try {
