d16af39205ac83a8db1f9e76c5c28108a7d9e7c0
ahcclubiium <ahcclubiium@gmail.com>
2024-07-10 15:47:34 +0800

    Update page.jsx

--- app/program_cms/page.jsx
+++ app/program_cms/page.jsx
@@ -1,62 +1,35 @@
-'use client'
-import React, { useState, useEffect } from "react";
-import BlogPostCard from "../components/BlogPostCard";
-import Heading from "../components/Heading";
-import { createClient } from "contentful";
 
-export const revalidate = 10;
+import BlogPostCard from '../components/BlogPostCard';
+import Heading from '../components/Heading';
+import { createClient } from 'contentful'; 
 
-const CONTENTFUL_SPACE_ID='fb9e3gkpjnm6'
-const CONTENTFUL_ACCESS_KEY='YFXxdZmCP54QU2G_lgH0EunE2NyjJO2vILRCbuUn0FY'
-
-export default function Page() {
-  const [entries, setEntries] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const client = createClient({
-      space: CONTENTFUL_SPACE_ID,
-      accessToken: CONTENTFUL_ACCESS_KEY,
-    });
-
-    client.getEntries({ content_type: "program" })
-      .then((response) => {
-        console.log("Fetched Entries:", response.items);
-        setEntries(response.items); // Update state with fetched items
-      })
-      .catch((error) => {
-        console.error("Error fetching Contentful data:", error);
-        setError(error);
-      });
-  }, []); // Empty dependency array ensures useEffect runs once on component mount
-
+// const CONTENTFUL_SPACE_ID = process.env.REACT_APP_CONTENTFUL_SPACE_ID;
+// const CONTENTFUL_ACCESS_TOKEN = process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN;
+export default async function Page() {
+   // DON'T TOUCH THIS PART
+  const client = await createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    environment: "master",
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN, 
+  });
+  
   return (
     <section>
-      <div>
-          <div className="my-36 bg-[#FEB7A9] ">
-         <h2 className={"text-5xl text-[#8B2813] font-extrabold ml-64 py-10"}>
-           <Heading text={"Programs"}/>
-         </h2>
-         </div>
+      <div className="text-center my-7">
+        <Heading text={'Programs'} />
+      </div>
       <div className="lg:grid grid-cols-2 gap-4 gap-y-10 lg:mx-60 m-4">
-        {entries.map((entry) => (
+        {client
+          .getEntries({ content_type: "program" })
+          .then((entries) =>
+            entries.items.map((entry) => (
           <BlogPostCard
             key={entry.sys.id}
             title={entry.fields.title}
-            thumbnail={entry.fields.thumbnail?.fields?.file?.url || ""}
+            thumbnail={entry.fields.thumbnail?.fields?.file?.url || ''}
             desc={entry.fields.description}
           />
-        ))}
-      </div>
-      {error && (
-        <div className="text-center my-7">
-          <Heading text={"Error fetching data"} />
-          <div className="mt-3">
-            <p className="text-red-500">Details:</p>
-            <p>{error.message}</p> {/* Display error message */}
-          </div>
-        </div>
-      )}
+        )))}
       </div>
     </section>
   );
