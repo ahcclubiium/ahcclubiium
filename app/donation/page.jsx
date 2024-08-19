73588c6257a2ca1898e22016570f9a29305d0b84
NaqiyyahMurshidah <naqiyyahmurshidah1@gmail.com>
2024-08-19 10:16:56 +0800

    latest design
    latest deisgn of donation

--- app/donation/page.jsx
+++ app/donation/page.jsx
@@ -9,10 +9,10 @@ export default function donation() {
       <Donation></Donation>
       <div className="">
         {/* the ways to donate section */}
-        <div className="flex flex-row ml-[18rem] mt-20 items-center ">
-          <h1 className="text-[#B99771] text-[5rem] font-semibold mr-14">
+        <div className="flex flex-row ml-[18rem] mt-20  ">
+          <h1 className="text-[#B99771] text-[3rem] font-semibold mr-14">
             {" "}
-            How to <br /> donate
+            How to donate
           </h1>
           {/* first container */}
           <div className="bg-[#FFD878] text-black h-[22rem] z-0 relative w-[29rem] ">
@@ -22,16 +22,16 @@ export default function donation() {
             </p>
           </div>
           {/* sec container */}
-          <div className="bg-[#F6F2CC] text-black h-fit mt-[5.7rem] p-4 z-10 absolute ml-[24rem] shadow-2xl">
+          <div className="bg-[#F6F2CC] text-black h-fit mt-[9rem] p-4 z-10 absolute ml-[29rem] shadow-2xl">
             <p>Phone: 123-456-7890</p>
-            <p>Bnak Details:</p>
+            <p>Bank Details:</p>
             <p>Bank Name: Maybank</p>
             <p>Account Number: 5622 0965 6727</p>
             <p>Account Name: Persatuan Kebajikan Haiwan Abu Hurairah </p>
           </div>
         </div>
 
-        <div className=" flex flex-col justify-center items-center lg:mt-32 text-center bg-[#B89670]">
+        <div className=" flex flex-col justify-center items-center lg:mt-24 text-center bg-[#B89670]">
           <p
             className="text-white mt-10 font-semibold text-[3rem]"
             style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
